396379930418834a1ca9900b07ffbe8cc9ab3500
Carlos Henrique de Oliveira <carlos.h.oliveira@cho.eti.br>
2021-01-15 12:49:59 -0300

    Início
    Criação do projeto ms-livro utilizando o Spring Initializer
    Adição do git ignore à raiz do projeto

---
+++ ms-livro/src/main/java/com/reading/mslivro/MsLivroApplication.java
@@ -0,0 +1,13 @@
+package com.reading.mslivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLivroApplication.class, args);
+	}
+
+}
---
+++ ms-livro/src/test/java/com/reading/mslivro/MsLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reading.mslivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
