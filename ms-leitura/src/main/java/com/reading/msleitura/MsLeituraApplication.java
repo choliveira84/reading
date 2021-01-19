a92e02faa54fd94bc73f928aeeb8308f6c906479
Carlos Henrique de Oliveira <carlos.h.oliveira@cho.eti.br>
2021-01-19 00:48:27 -0300

    Leitura
    Criação de projeto

---
+++ ms-leitura/src/main/java/com/reading/msleitura/MsLeituraApplication.java
@@ -0,0 +1,13 @@
+package com.reading.msleitura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLeituraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLeituraApplication.class, args);
+	}
+
+}
