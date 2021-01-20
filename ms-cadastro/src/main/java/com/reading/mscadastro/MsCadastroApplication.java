bf60dd4c517dd469d5bae197a1689680161de929
Carlos Henrique de Oliveira <carlos.h.oliveira@cho.eti.br>
2021-01-20 01:43:10 -0300

    Eureka Clint
    Portas aleatórios
    Configuração do Eureka Client

--- ms-cadastro/src/main/java/com/reading/mscadastro/MsCadastroApplication.java
+++ ms-cadastro/src/main/java/com/reading/mscadastro/MsCadastroApplication.java
@@ -2,10 +2,12 @@ package com.reading.mscadastro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class MsCadastroApplication {
