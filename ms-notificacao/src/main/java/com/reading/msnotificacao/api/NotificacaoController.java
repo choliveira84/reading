bb9fffcfc072825cd8249ec58e7eb8a0d977ad7c
Carlos Henrique de Oliveira <carlos.h.oliveira@cho.eti.br>
2021-01-22 01:47:01 -0300

    Api Gateway Zuul
    Criação do projeto
    Alteração da porta para aleatória nos projetos
    Remoção do sleep

--- ms-notificacao/src/main/java/com/reading/msnotificacao/api/NotificacaoController.java
+++ ms-notificacao/src/main/java/com/reading/msnotificacao/api/NotificacaoController.java
@@ -41,73 +41,57 @@ public class NotificacaoController {
         @PostMapping("/resenha")
         public ResponseEntity<NotificacaoDTO> criar(@Valid @RequestBody NotificacaoResenhaDTO request)
                         throws URISyntaxException {
+                NotificacaoDTO notificacao = service.criar(request);
 
-                try {
-                        Thread.sleep(60001l);
-                } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                }
+                HttpHeaders headers = new HttpHeaders();
+                headers.add("X-msnotificacao-created", "Notificação criada.");
+                return ResponseEntity.created(new URI("/api/notificacoes/" + notificacao.getId())).headers(headers)
+                                .body(notificacao);
+        }
 
-        NotificacaoDTO notificacao = service.criar(request);
+        @ApiResponses(value = {
+                        @ApiResponse(code = 201, message = "Response com a notificação criada para o tipo Livro Publicado"),
+                        @ApiResponse(code = 404, message = "Livro não encontrato") })
+        @PostMapping("/livro")
+        public ResponseEntity<NotificacaoDTO> criar(@Valid @RequestBody NotificacaoLivroPublicadoDTO request)
+                        throws URISyntaxException {
+                NotificacaoDTO notificacao = service.criar(request);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("X-msnotificacao-created", "Notificação criada.");
-        return ResponseEntity.created(new URI("/api/notificacoes/" + notificacao.getId())).headers(headers)
-                .body(notificacao);
-    }
+                HttpHeaders headers = new HttpHeaders();
+                headers.add("X-msnotificacao-created", "Notificação criada.");
+                return ResponseEntity.created(new URI("/api/notificacoes/" + notificacao.getId())).headers(headers)
+                                .body(notificacao);
+        }
 
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Response com a notificação criada para o tipo Livro Publicado"),
-            @ApiResponse(code = 404, message = "Livro não encontrato") })
-    @PostMapping("/livro")
-    public ResponseEntity<NotificacaoDTO> criar(@Valid @RequestBody NotificacaoLivroPublicadoDTO request)
-            throws URISyntaxException {
+        @ApiResponses(value = { @ApiResponse(code = 200, message = "Response com a notificação") })
+        @GetMapping
+        public ResponseEntity<List<NotificacaoDTO>> buscar(NotificacaoBuscaDTO request, Pageable pageable) {
+                Page<NotificacaoDTO> page = service.buscar(request, pageable);
 
-                try {
-                        Thread.sleep(60001l);
-                } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                }
+                HttpHeaders headers = new HttpHeaders();
+                headers.add("X-Total-Count", Long.toString(page.getTotalElements()));
 
-        NotificacaoDTO notificacao = service.criar(request);
+                return ResponseEntity.ok().headers(headers).body(page.getContent());
+        }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("X-msnotificacao-created", "Notificação criada.");
-        return ResponseEntity.created(new URI("/api/notificacoes/" + notificacao.getId())).headers(headers)
-                .body(notificacao);
-    }
+        @ApiResponses(value = { @ApiResponse(code = 204, message = "Sem conteúdo"),
+                        @ApiResponse(code = 404, message = "Notificação não encontrada") })
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Void> deletar(@PathVariable Long id) {
+                service.deletar(id);
 
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "Response com a notificação") })
-    @GetMapping
-    public ResponseEntity<List<NotificacaoDTO>> buscar(NotificacaoBuscaDTO request, Pageable pageable) {
-        Page<NotificacaoDTO> page = service.buscar(request, pageable);
+                HttpHeaders headers = new HttpHeaders();
+                headers.add("X-msnotificacao-deleted", "Notificação deletada.");
+                return ResponseEntity.noContent().headers(headers).build();
+        }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("X-Total-Count", Long.toString(page.getTotalElements()));
+        @ApiResponses(value = { @ApiResponse(code = 200, message = "Response com a notificação"),
+                        @ApiResponse(code = 404, message = "Notificação não encontrada") })
+        @GetMapping("/{id}")
+        public ResponseEntity<NotificacaoDTO> consultar(@PathVariable Long id) {
+                NotificacaoDTO notificacao = service.consultar(id);
 
-        return ResponseEntity.ok().headers(headers).body(page.getContent());
-    }
-
-    @ApiResponses(value = { @ApiResponse(code = 204, message = "Sem conteúdo"),
-            @ApiResponse(code = 404, message = "Notificação não encontrada") })
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletar(@PathVariable Long id) {
-        service.deletar(id);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("X-msnotificacao-deleted", "Notificação deletada.");
-        return ResponseEntity.noContent().headers(headers).build();
-    }
-
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "Response com a notificação"),
-            @ApiResponse(code = 404, message = "Notificação não encontrada") })
-    @GetMapping("/{id}")
-    public ResponseEntity<NotificacaoDTO> consultar(@PathVariable Long id) {
-        NotificacaoDTO notificacao = service.consultar(id);
-
-        return ResponseEntity.ok().body(notificacao);
-    }
+                return ResponseEntity.ok().body(notificacao);
+        }
 
 }
